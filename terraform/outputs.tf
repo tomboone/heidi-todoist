86a0db2042e2df6933fde44a10ba9e6df5b79e86
Tom Boone <tbc@tomboone.com>
2026-04-25 12:41:03 -0400

    remove infra/ and .github/

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "function_app_name" {
-  value = azurerm_linux_function_app.main.name
-}
-
-output "python_version" {
-  value       = azurerm_linux_function_app.main.site_config[0].application_stack[0].python_version
-  description = "The Python version configured for the app service"
-}
